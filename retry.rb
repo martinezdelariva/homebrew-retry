f29c27a18811b1b7f36a97ab2a5993aa7a5fe67d
José Luis Martínez de la Riva <martinezdelariva@gmail.com>
2019-02-16 19:21:35 +0100

    release v0.2.2

--- retry.rb
+++ retry.rb
@@ -1,8 +1,8 @@
 class Retry < Formula
   desc "The missing command-line tool to execute the same command several times"
   homepage "https://github.com/martinezdelariva/retry"
-  url "https://github.com/martinezdelariva/retry/releases/download/v0.2/retry-mac"
-  sha256 "0a5a859bf0b8c108d3cd5bc5aaa520848138dcd07fd3d9ea8005dd899122df17"
+  url "https://github.com/martinezdelariva/retry/releases/download/v0.2.2/retry-mac"
+  sha256 "5aac3953a4c2ce0ce874cb6c0c97a81d58664c3961f4b1787cf11ca672750d19"
 
   def install
     system "mv", "retry-mac", "retry"
