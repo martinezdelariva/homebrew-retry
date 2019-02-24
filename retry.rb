8a4b283f728e131afeff2f771d9d428323595b55
José Luis Martínez de la Riva <martinezdelariva@gmail.com>
2019-02-24 10:46:07 +0100

    missing version

--- retry.rb
+++ retry.rb
@@ -1,5 +1,6 @@
 class Retry < Formula
   desc "The missing command-line tool to execute the same command several times"
+  version '0.3.0'
   homepage "https://github.com/martinezdelariva/retry"
   url "https://github.com/martinezdelariva/retry/releases/download/v0.3.0/retry-mac"
   sha256 "010d62d2fc1d496972f1fffcfa46073ccfaf1b8e0de9b19b3af027d166423e6f"
